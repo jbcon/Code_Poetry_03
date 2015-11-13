52ceddb16c8767ace7eca054c266f7294895b01f
John Conover <111jbcon@gmail.com>
2015-11-13 13:00:37 -0500

    Initial poetry commit

---
+++ poem/invictus.cpp
@@ -0,0 +1,29 @@
+#include <cassert>
+#include "invictus.hpp"
+
+Person me;
+
+int main(){
+    Night night;
+    night.covers(me);
+    night.blackness = "pit from pole to pole";
+    for (int i = 0; i < gods.size(); i++){
+        gods[i].thank("my unconquerable soul");
+    }
+
+    assert(!me.winced() && !me.cried("aloud"));
+    while(chance.bludgeoning(me)){
+        if (me.head.isBloody() && me.head.isUnbowed()){
+            break;
+        }
+    }
+
+    shade.horror.loom(beyond(Place("wrath", "tears")));
+
+    if (true || gate.isStrait() || scroll.isCharged("punishments")){
+        me.masterOf(me.fate);
+        me.captainOf(me.soul);
+    }
+
+    return 0;
+}
---
+++ poem/invictus.hpp
@@ -0,0 +1,66 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+class Head{
+public:
+    bool isUnbowed(){return true;}
+    bool isBloody(){return true;}
+};
+
+
+class Person{
+public:
+    bool winced(){return false;}
+    bool cried(string how){return false;}
+
+    bool masterOf(string ofWhat){}
+    bool captainOf(string ofWhat){}
+
+    Head head;
+    string fate;
+    string soul;
+};
+
+
+class Place{
+public:
+    Place(string trait, ...){}
+};
+
+class God{
+public:
+    void thank(string forWhat){}
+};
+
+std::vector<God> gods;
+
+class Night{
+public:
+    Night(){}
+    bool covers(Person p){return true;}
+    string blackness;
+};
+
+class Obstacle {
+public:
+    bool isStrait(){}
+    bool isCharged(string withWhat){}
+    bool bludgeoning(Person p){return true;}
+    void loom(float loc){}
+};
+
+class Shade{
+public:
+    Obstacle horror;
+};
+
+Shade shade;
+Obstacle gate;
+Obstacle scroll;
+Obstacle chance;
+
+void out_from(bool c){}
+float beyond(Place p){return INFINITY;}
